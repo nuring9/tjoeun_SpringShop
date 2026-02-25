5aa7cf91acd6f648a6ab0132581ef3bd1fc3c7c3
nuring9 <for9595@gmail.com>
2026-02-25 17:01:24 +0900

    item 갯수 조정

--- src/main/java/com/shop/controller/MainController.java
+++ src/main/java/com/shop/controller/MainController.java
@@ -19,7 +19,7 @@ public class MainController {
     private final ItemService itemService;
   @GetMapping(value = "/")
     public String main(ItemSearchDto itemSearchDto, Optional<Integer> page, Model model){
-      Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 5);
+      Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 6);
       Page<MainItemDto> items = itemService.getMainItemPage(itemSearchDto, pageable);
       model.addAttribute("items", items);
       model.addAttribute("itemSearchDto", itemSearchDto);
