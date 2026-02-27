b87615e691c22617e66a8981ab4e3c4ff0997ac8
nuring9 <for9595@gmail.com>
2026-02-27 15:29:52 +0900

    주문취소 기능완료

--- src/main/java/com/shop/service/OrderService.java
+++ src/main/java/com/shop/service/OrderService.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -47,18 +48,46 @@ public class OrderService {
         List<Order> orders = orderRepository.findOrders(email, pageable);
         Long totalCount = orderRepository.countOrder(email);
 
+        // View에 사용한 모델 Dto 객체 생성하기
         List<OrderHistDto> orderHistDtos = new ArrayList<>();
+        // 주문서 리스트에서 주문서 하나씩 빼기
+        // 언제까지? 없을때까지
 
         for(Order order : orders){
+            // Dto List에 입주할 OrderHistDto 객체를 생성 -> 생성자 주문서 Order를 넣는다.
             OrderHistDto orderHistDto = new OrderHistDto(order);
+            // List 담기
             List<OrderItem> orderItems = order.getOrderItems();
+            // 주문 아이템 =  주문 아이템들
+
             for(OrderItem orderItem : orderItems){
+                // ItemImg 대표이미지
                 ItemImg itemImg = itemImgRepository.findByItemIdAndRepImgYn(orderItem.getItem().getId(), "Y");
+                // OrderItemDto 주문 아이템 객체를 생성
+                // List에 담아야 되니까
+
                 OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                // 객체 만든 OrderHistDto 객체를 -> OrderHistDto -> List에 추가
                 orderHistDto.addOrderItemDto(orderItemDto);
             }
-            orderHistDtos.add(orderHistDto);
+            orderHistDtos.add(orderHistDto);  // 그 완성된 주문서 orderHistDto를 -> 주문서 리스트 orderHistDtos에 넣어줌.
         }
         return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
+    }
+   @Transactional(readOnly = true)
+   public boolean validateOrder(Long orderId, String email){
+        Member curMember = memberRepository.findByEmail(email);
+        Order order = orderRepository.findById(orderId).orElseThrow(EntityNotFoundException::new);
+        Member savedMember = order.getMember();
+
+        if(!StringUtils.equals(curMember.getEmail(), savedMember.getEmail())){
+            return false;
+        }
+        return true;
+   }
+
+    public void cancelOrder(Long orderId){
+        Order order = orderRepository.findById(orderId).orElseThrow(EntityNotFoundException::new);
+        order.cancelOrder();
     }
 }
